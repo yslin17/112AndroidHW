774891b48c4b565478d1d72a4ff4e5af8220ca9a
yslin17 <betty20031217@gmail.com>
2024-03-28 22:52:51 +0800

    0316

--- HWCounter/app/src/main/java/com/example/hwcounter/MainActivity.java
+++ HWCounter/app/src/main/java/com/example/hwcounter/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.hwcounter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void button_Click(View view)
+    {
+        int count;
+        TextView output = (TextView) findViewById(R.id.textView);
+        count = Integer.parseInt(output.getText().toString());
+        count--;
+        output.setText(Integer.toString(count));
+    }
+
+    public void button2_Click(View view)
+    {
+        TextView output = (TextView) findViewById(R.id.textView);
+        output.setText(R.string.ini_value);
     }
 }
--- HWLogin/app/src/main/java/com/example/hwlogin/MainActivity.java
+++ HWLogin/app/src/main/java/com/example/hwlogin/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.hwlogin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public static final String DEFAULT_Account = "A111222028";
+    public static final String DEFAULT_Password = "0000";
+
+    public void button_Click(View view)
+    {
+        EditText edtAccount = (EditText) findViewById(R.id.edtAccount);
+        EditText edtPassword = (EditText) findViewById(R.id.edtPassword);
+        TextView txvShow = (TextView) findViewById(R.id.txvShow);
+
+        String username = edtAccount.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        if (username.equals(DEFAULT_Account) && password.equals(DEFAULT_Password))
+        {
+            txvShow.setText("登入成功");
+        } else if (username.equals(DEFAULT_Account))
+        {
+            txvShow.setText("密碼錯誤，登入失敗");
+        } else if (password.equals(DEFAULT_Password))
+        {
+            txvShow.setText("帳號錯誤，登入失敗");
+        } else
+        {
+            txvShow.setText("帳號和密碼錯誤，登入失敗");
+        }
     }
 }
