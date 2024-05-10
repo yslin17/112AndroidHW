fcfc24a2942f341914e4cd685f338a1692ea2d27
yslin17 <betty20031217@gmail.com>
2024-05-10 11:34:41 +0800

    0503

---
+++ FoodImgMenu_0503HW/app/src/main/java/com/example/foodimgmenu_0503hw/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodimgmenu_0503hw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ FoodImgMenu_0503HW/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FoodImgMenu_0503HW"
+include(":app")
+ 
